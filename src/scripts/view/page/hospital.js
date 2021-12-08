4ef44c4177bfd745c323cf99bc783e6f6b51470f
Zulham Ari <ari.comunity1@gmail.com>
2021-12-08 15:15:42 +0700

    add mapview

--- src/scripts/view/page/hospital.js
+++ src/scripts/view/page/hospital.js
@@ -1,12 +1,28 @@
 const RumahSakit = {
   async render () {
     return `
-            <h1> RS.Rujukan </h1>
-        `
+      <section class="map-box">
+        <div id="map"></div>
+      <section>
+    `
   },
 
   async afterRender () {
+    const map = L.map('map').setView([51.505, -0.09], 13)
 
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    })
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map)
+
+    L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup()
   }
 }
 export default RumahSakit
--- src/scripts/view/page/zone.js
+++ src/scripts/view/page/zone.js
@@ -1,12 +1,31 @@
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
 const Zona = {
   async render () {
     return `
-            <h1> Zona </h1>
-        `
+      <section class="map-box">
+        <div id="map"></div>
+      <section>
+    `
   },
 
   async afterRender () {
+    const map = L.map('map').setView([51.505, -0.09], 13)
 
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    })
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map)
+
+    L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup()
   }
 }
 export default Zona
