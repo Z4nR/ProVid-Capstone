15437c9742cf76ffe1fc3e07493bc6918c6f0ff9
Zulham Ari <ari.comunity1@gmail.com>
2021-12-09 13:13:23 +0700

    add geojson mapping

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
+  MapBox_Token: 'pk.eyJ1IjoiejRuciIsImEiOiJja2pzand4cW4xMm45Mnl0Y2RvdjJtZHYxIn0.Rn_hw5jHlwnMJ5BVcQ8egg',
   COVID_URL: '',
   ZONE_URL: '',
   HOSPITAL_URL: '',
--- src/scripts/view/page/zone.js
+++ src/scripts/view/page/zone.js
@@ -1,4 +1,6 @@
 import L from 'leaflet'
+import province from '../../data/prov_id'
+import CONFIG from '../../globals/config'
 
 const Zona = {
   async render () {
@@ -20,9 +22,14 @@ const Zona = {
       shadowUrl: require('leaflet/dist/images/marker-shadow.png')
     })
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + CONFIG.MapBox_Token, {
+      id: 'mapbox/light-v9',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      tileSize: 512,
+      zoomOffset: -1
     }).addTo(map)
+
+    L.geoJson(province).addTo(map)
   }
 }
 export default Zona
